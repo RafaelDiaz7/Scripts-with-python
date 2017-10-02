cdc4061b3bad5bc0ed0a54954056ae767e6752cf
Rafael Diaz ;) <rafaelaarondiaz123@gmail.com>
2017-10-01 22:05:03 -0400

    Api for download pictures from lorempixels
    Its great!

---
+++ apidemo.py
@@ -0,0 +1,26 @@
+from urllib2 import urlopen
+
+url = 'http://lorempixel.com/'
+categorias = ['abstract','animals','business','cats','city',
+			  'food','night','life','fashion','people',
+			  'nature','sports','technics','transport']
+
+print "Categorias"
+print "=" * 9
+print "\n"
+
+for i in categorias:
+	print i 
+
+print "\n"
+
+alto = raw_input('Escribe la altura de la imagen: ')
+ancho = raw_input('Escribe la anchura de la imagen: ')
+categoria = raw_input('Escribe la categoria de la imagen que buscas:' )
+
+url_req = "%s/%s/%s/%s" % (url,ancho,alto,categorias[int(categoria)])
+resultado = urlopen(url_req)
+lectura = resultado.read()
+f = open("holder_%d.jpg" % img, "wb")#foto
+f.write(lectura)
+f.close()
