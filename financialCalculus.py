992814789f89621649c8c4fa10180e2f0538cb4d
Rafael Diaz ;) <rafaelaarondiaz123@gmail.com>
2017-11-09 21:30:38 -0400

    Financial calculating tools

---
+++ financialCalculus.py
@@ -0,0 +1,13 @@
+def roi(ganancia, inversion):
+	return (ganancia/inversion)*100
+
+def riqueza_en_tiempo(activos_liquidos, ingresos):
+	return (activos_liquidos/gastos_mensuales)/12
+
+def ingresos_por_activos(ingresos_financieros, ingresos_totales)
+	return (ingresos_financieros/ingresos_totales)*100
+
+print "=============================="
+print "Dato: Una buena tecnica para ahorrar es guardar el 30"+str(%)+" de tus ganancias mensuales."
+
+print "Tu inversion retornada"roi(100000, 5000)
